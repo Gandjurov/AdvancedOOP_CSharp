682452c4f44ee9166769b07aa02a394f80b19bd0
Gandjurov <gandjurov@abv.bg>
2018-12-12 15:31:58 +0200

    add Injector.cs

---
+++ 06.Workshop-DependancyInjention/01.SoftUniDI/Injectors/Injector.cs
@@ -0,0 +1,17 @@
+﻿using SoftUniDI.Modules.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniDI.Injectors
+{
+    public class Injector
+    {
+        private readonly IModule module;
+
+        public Injector(IModule module)
+        {
+            this.module = module;
+        }
+    }
+}
