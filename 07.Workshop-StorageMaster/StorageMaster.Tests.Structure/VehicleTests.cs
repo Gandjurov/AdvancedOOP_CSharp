aa4dd8396d5db2774ef7736b353fde0ddf708ab2
Gandjurov <gandjurov@abv.bg>
2018-12-13 12:57:27 +0200

    Add test structute(unitTests) and starting tests for Vehicles

---
+++ 07.Workshop-StorageMaster/StorageMaster.Tests.Structure/VehicleTests.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using StorageMaster.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StorageMaster.Tests.Structure
+{
+    public class VehicleTests
+    {
+        //Validate entities
+        [Test]
+        public void ValidateAllVehicles()
+        {
+            var types = new[]
+            {
+                "Semi",
+                "Truck",
+                "Van",
+                "Vehicle"
+            };
+
+            foreach (var type in types)
+            {
+                var currentType = GetType(type);
+
+                Assert.That(currentType, Is.Not.Null, $"{type} doesn't exists");
+            }
+        }
+
+        [Test]
+        public void ValidateVehicleProperties()
+        {
+            //typeof(Vehicle)
+            var vehicleType = GetType("Vehicle");
+
+            var actualProperties = vehicleType.GetProperties();
+
+            var expectedProperties = new Dictionary<string, Type>
+            {
+                { "Capacity", typeof(int) },
+                { "Trunk", typeof(IReadOnlyCollection<Product>) },
+                { "IsFull", typeof(bool) },
+                { "IsEmpty", typeof(bool) }
+            };
+
+            //new[] { "Capacity", "Trunk", "IsFull", "IsEmpty" };
+
+            //return type
+
+
+            foreach (var actualProperty in actualProperties)
+            {
+                var isValidProperty = expectedProperties.Any(x => x.Key == actualProperty.Name &&
+                                      actualProperty.PropertyType == x.Value);
+
+                Assert.That(isValidProperty, $"{actualProperty.Name} doesn't exists!");
+            }
+        }
+
+        //Existing Methods:
+        //void LoadProduct(Product product)
+        //Product Unload()
+        [Test]
+        public void ValidateVehicleMethods()
+        {
+
+        }
+
+
+        private Type GetType(string type)
+        {
+            var targetType = typeof(StartUp)
+                .Assembly
+                .GetTypes()
+                .FirstOrDefault(x => x.Name == type);
+
+            return targetType;
+        }
+    }
+}
