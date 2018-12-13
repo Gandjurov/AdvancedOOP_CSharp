15e4d3ec26fe8d472f0ca154ba2646f13e39ee69
Gandjurov <gandjurov@abv.bg>
2018-12-13 09:53:04 +0200

    Add Survice Provider

---
+++ 06.Workshop-DependancyInjention/SurviceProvider/IServiceCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SurviceProvider
+{
+    public interface IServiceCollection
+    {
+        void AddService<TImplementation, TClass>();
+
+        void CreateInstance<TImplementation>();
+
+
+    }
+}
