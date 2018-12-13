27c2fe82cea5a4e6013811ddba9c85f830118cf1
Gandjurov <gandjurov@abv.bg>
2018-12-13 15:36:15 +0200

    temporary update

--- 07.Workshop-StorageMaster/StorageMaster.Tests.Structure/VehicleTests.cs
+++ 07.Workshop-StorageMaster/StorageMaster.Tests.Structure/VehicleTests.cs
@@ -64,7 +64,46 @@ namespace StorageMaster.Tests.Structure
         [Test]
         public void ValidateVehicleMethods()
         {
+            var vehicleType = GetType("Vehicle");
 
+            var expectedMethods = new List<Method>
+            {
+                new Method(typeof(void), "LoadProduct", typeof(Product)),
+                new Method(typeof(Product), "Unload")
+            };
+
+            var loadProductMethod = vehicleType.GetMethod("LoadProduct");
+            var unloadMethod = vehicleType.GetMethod("Unload");
+
+            Assert.That(loadProductMethod, Is.Not.Null, "LoadProduct method doesn't exists");
+            Assert.That(unloadMethod, Is.Not.Null, "Unload method doesn't exists");
+
+            var loadProductReturnType = loadProductMethod.ReturnType == typeof(void);
+            var unloadReturnType = unloadMethod.ReturnType == typeof(Product);
+
+            Assert.That(loadProductReturnType, $"LoadProduct invalid return type");
+            Assert.That(unloadReturnType, $"Unload invalid return type");
+            //foreach (var actualMethod in actualMethods)
+            //{
+            //    var actualReturnType = actualMethod.ReturnType;
+            //    var actualName = actualMethod.Name;
+            //    var actualParams = actualMethod.GetParameters();
+
+            //    var expectedMethod = expectedMethods.FirstOrDefault(x => x.Name == actualName);
+
+            //    Assert.That(expectedMethod, Is.Not.Null, $"{expectedMethod.Name} doesn't exists");
+
+            //    var isValidReturnType = expectedMethod.ReturnType == actualReturnType;
+
+            //    Assert.That(isValidReturnType, $"{actualMethod.Name} Invalid return type");
+
+            //    foreach (var actualParam in actualParams)
+            //    {
+            //        var isValidParams = expectedMethod.InputParameters.Any(x => x.Name == actualParam.Name);
+
+            //        Assert.That(isValidParams, $"{actualMethod.Name} Missing parameter");
+            //    }
+            //}
         }
 
 
@@ -76,6 +115,21 @@ namespace StorageMaster.Tests.Structure
                 .FirstOrDefault(x => x.Name == type);
 
             return targetType;
+        }
+
+        private class Method
+        {
+            public Method(Type returnType, string name, params Type[] inputParams)
+            {
+                this.ReturnType = returnType;
+                this.Name = name;
+                this.InputParameters = inputParams;
+            }
+            public Type ReturnType { get; set; }
+
+            public string Name { get; set; }
+            
+            public Type[] InputParameters { get; set; }
         }
     }
 }
