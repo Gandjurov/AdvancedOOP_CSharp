61f92f48d0ff92dccaebb7eadfca1c8043e9e424
Gandjurov <gandjurov@abv.bg>
2018-12-13 23:17:17 +0200

    ValidateAddProductMethod test is done

--- 07.Workshop-StorageMaster/StorageMaster.BusinessLogic.Tests/StorageMasterTests.cs
+++ 07.Workshop-StorageMaster/StorageMaster.BusinessLogic.Tests/StorageMasterTests.cs
@@ -1,11 +1,47 @@
-﻿using NUnit.Framework;
-using System;
-
-namespace StorageMaster.BusinessLogic.Tests
+﻿namespace StorageMaster.BusinessLogic.Tests
 {
+    using NUnit.Framework;
+    using StorageMaster.Entities.Products;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
     [TestFixture]
     public class StorageMasterTests
     {
+        [Test]
+        public void ValidateAddProductMethod()
+        {
+            var storageMasterType = this.GetType("StorageMaster");
+            var addProductMethod = storageMasterType.GetMethod("AddProduct");
 
+            var instance = Activator.CreateInstance(storageMasterType);
+
+            string productType = "SolidStateDrive";
+            double price = 230.43;
+
+            var actualResult = addProductMethod.Invoke(instance, new object[] { productType, price });
+            var expectedResult = $"Added SolidStateDrive to pool";
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+            var productPoolField = (IDictionary<string, Stack<Product>>)storageMasterType
+                                            .GetField("productsPool", (BindingFlags)62)
+                                            .GetValue(instance);
+            
+            Assert.That(productPoolField[productType].Count, Is.EqualTo(1));
+        }
+
+        private Type GetType(string type)
+        {
+            var targetType = typeof(StartUp)
+                .Assembly
+                .GetTypes()
+                .FirstOrDefault(x => x.Name == type);
+
+            return targetType;
+        }
     }
 }
