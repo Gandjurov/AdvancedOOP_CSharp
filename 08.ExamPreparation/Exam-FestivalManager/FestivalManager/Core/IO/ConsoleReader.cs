b7279aad8b7449401402eaa539ead8c64a03b379
Gandjurov <gandjurov@abv.bg>
2018-12-14 11:56:50 +0200

    Adding ConsoleReader.cs and ConsoleWriter.cs Deleting FileReader.cs and StringReader.cs

---
+++ 08.ExamPreparation/Exam-FestivalManager/FestivalManager/Core/IO/ConsoleReader.cs
@@ -0,0 +1,15 @@
+﻿namespace FestivalManager.Core.IO
+{
+    using FestivalManager.Core.IO.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
