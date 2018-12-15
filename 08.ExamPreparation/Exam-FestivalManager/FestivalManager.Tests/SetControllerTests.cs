a4e8e7ba83f200889e3152d38dfadbb4c5bcae1e
Gandjurov <gandjurov@abv.bg>
2018-12-15 17:35:01 +0200

    Add second test

--- 08.ExamPreparation/Exam-FestivalManager/FestivalManager.Tests/SetControllerTests.cs
+++ 08.ExamPreparation/Exam-FestivalManager/FestivalManager.Tests/SetControllerTests.cs
@@ -1,18 +1,21 @@
+using FestivalManager.Core.Controllers;
+using FestivalManager.Core.Controllers.Contracts;
+using FestivalManager.Entities;
+using FestivalManager.Entities.Contracts;
+using FestivalManager.Entities.Sets;
+using FestivalManager.Entities.Instruments;
+using System;
+
 namespace FestivalManager.Tests
 {
-    using FestivalManager.Core.Controllers;
-    using FestivalManager.Core.Controllers.Contracts;
-    using FestivalManager.Entities;
-    using FestivalManager.Entities.Contracts;
-    using FestivalManager.Entities.Sets;
     using NUnit.Framework;
-    using System.Collections.Generic;
+    using System;
 
     [TestFixture]
 	public class SetControllerTests
     {
 		[Test]
-	    public void Test()
+	    public void SetControllerShouldReturnFailMessage()
 	    {
             IStage stage = new Stage();
             ISetController setController = new SetController(stage);
@@ -25,5 +28,28 @@ namespace FestivalManager.Tests
 
             Assert.That(actualResult, Is.EqualTo(expectedResult));
         }
-	}
+
+        [Test]
+        public void SetControllerShouldReturnSuccessMessage()
+        {
+            IStage stage = new Stage();
+            ISetController setController = new SetController(stage);
+
+            ISet set = new Short("Set1");
+            IPerformer performer = new Performer("Pesho", 12);
+            performer.AddInstrument(new Guitar());
+            set.AddPerformer(performer);
+
+            ISong song = new Song("Song", new TimeSpan(0, 2, 30));
+            set.AddSong(song);
+
+            stage.AddSet(set);
+
+            string expectedResult = "1. Set1:\r\n-- 1. Song (02:30)\r\n-- Set Successful";
+            string actualResult = setController.PerformSets();
+
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+    }
+
 }
