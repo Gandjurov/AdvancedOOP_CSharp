664cf12335738881fdc7f4d999ab10390f197b79
Gandjurov <gandjurov@abv.bg>
2018-12-18 20:47:39 +0200

    05.GenericCountMethodStrings

---
+++ 02.Generics/09.CustomListIterator/ISoftUniList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomListIterator
+{
+    public interface ISoftUniList<T> : IEnumerable<T>
+    {
+        int Count { get; }
+
+        T this[int number] { get; set; }
+
+        void Add(T element);
+
+        T Remove(int index);
+
+        bool Contains(T element);
+
+        void Swap(int index1, int index2);
+
+        int CountGreaterThan(T element);
+
+        T Max();
+
+        T Min();
+
+        void Sort();
+    }
+}
---
+++ 02.Generics/11.Threeuple/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Threeuple
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] personInfo = Console.ReadLine().Split();
+
+            string fullname = personInfo[0] + " " + personInfo[1];
+            string neighbours = personInfo[2];
+            string town = personInfo[3];
+
+            string[] beerInfo = Console.ReadLine().Split();
+
+            string name = beerInfo[0];
+            int liters = int.Parse(beerInfo[1]);
+            bool isDrunk = beerInfo[2] == "drunk" ? true : false;
+
+            string[] sashoInfo = Console.ReadLine().Split();
+
+            string sashoName = sashoInfo[0];
+            double sashoDouble = double.Parse(sashoInfo[1]);
+            string sashoBanka = sashoInfo[2];
+
+            SpecialTuple<string, string, string> personTuple = new SpecialTuple<string, string, string>(fullname, neighbours, town);
+            SpecialTuple<string, int, bool> beerTuple = new SpecialTuple<string, int, bool>(name, liters, isDrunk);
+            SpecialTuple<string, double, string> sashoTuple = new SpecialTuple<string, double, string>(sashoName, sashoDouble, sashoBanka);
+
+            Console.WriteLine(personTuple);
+            Console.WriteLine(beerTuple);
+            Console.WriteLine(sashoTuple);
+        }
+    }
+}
