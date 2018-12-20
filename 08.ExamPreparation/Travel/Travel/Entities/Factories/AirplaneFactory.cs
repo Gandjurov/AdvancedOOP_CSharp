caf0cea99f6313bd98df78406176968c24f98277
Gandjurov <gandjurov@abv.bg>
2018-12-21 01:55:39 +0200

    update AirplaneFactory and ItemFactory classes

--- 08.ExamPreparation/Travel/Travel/Entities/Factories/AirplaneFactory.cs
+++ 08.ExamPreparation/Travel/Travel/Entities/Factories/AirplaneFactory.cs
@@ -3,20 +3,19 @@
 	using Contracts;
 	using Airplanes.Contracts;
     using Entities.Airplanes;
+    using System.Reflection;
+    using System.Linq;
+    using System;
 
     public class AirplaneFactory : IAirplaneFactory
 	{
 		public IAirplane CreateAirplane(string type)
 		{
-			switch (type)
-			{
-				case "LightAirplane":
-					return new LightAirplane();
-				case "MediumAirplane":
-					return new MediumAirplane();
-				default:
-					return null; //TODO what to return
-			}
-		}
+            var airplaneType = Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(x => x.Name == type);
+            var airplaneInstance = (IAirplane)Activator.CreateInstance(airplaneType);
+
+            return airplaneInstance;
+
+        }
 	}
 }
--- 08.ExamPreparation/Travel/Travel/Entities/Factories/ItemFactory.cs
+++ 08.ExamPreparation/Travel/Travel/Entities/Factories/ItemFactory.cs
@@ -3,28 +3,18 @@
 	using Contracts;
 	using Items;
 	using Items.Contracts;
+    using System;
+    using System.Linq;
+    using System.Reflection;
 
-	public class ItemFactory : IItemFactory
+    public class ItemFactory : IItemFactory
 	{
 		public IItem CreateItem(string type)
 		{
-			switch (type)
-			{
-				case "CellPhone":
-					return new CellPhone();
-				case "Colombian":
-					return new Colombian();
-				case "Jewelery":
-					return new Jewelery();
-				case "Laptop":
-					return new Laptop();
-				case "Toothbrush":
-					return new Toothbrush();
-				case "TravelKit":
-					return new TravelKit();
-				default:
-                    return null; //TODO what to do
-			}
-		}
+            var itemType = Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(x => x.Name == type);
+            var itemInstance = (IItem)Activator.CreateInstance(itemType);
+
+            return itemInstance;
+        }
 	}
 }
