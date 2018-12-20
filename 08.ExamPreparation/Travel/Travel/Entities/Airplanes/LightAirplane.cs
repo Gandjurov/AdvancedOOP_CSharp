5f9744ccf4bfc7e22bc92612b153f9301cc2c8c2
Gandjurov <gandjurov@abv.bg>
2018-12-20 23:31:43 +0200

    add LightAirplane.cs and MediumAirplane.cs

---
+++ 08.ExamPreparation/Travel/Travel/Entities/Airplanes/LightAirplane.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Travel.Entities.Airplanes
+{
+    public class LightAirplane : Airplane
+    {
+        private const int LightSeats = 5;
+        private const int LightBaggageCompartments = 8;
+
+        public LightAirplane() 
+            : base(LightSeats, LightBaggageCompartments)
+        {
+        }
+    }
+}
---
+++ 08.ExamPreparation/Travel/Travel/Entities/Airplanes/MediumAirplane.cs
@@ -0,0 +1,17 @@
+﻿namespace Travel.Entities.Airplanes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class MediumAirplane : Airplane
+    {
+        private const int MediumAirplaneSeats = 10;
+        private const int MediumAirplaneBaggageCompartments = 14;
+
+        public MediumAirplane() 
+            : base(MediumAirplaneSeats, MediumAirplaneBaggageCompartments)
+        {
+        }
+    }
+}
