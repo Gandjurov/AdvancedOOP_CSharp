120a8ae03443956f716737d2df269c0927b4248c
Gandjurov <gandjurov@abv.bg>
2018-12-20 20:37:07 +0200

    add 02.Collection

---
+++ 03.IteratorsAndComparators/02.Collection/ListyIterator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private T[] elements;
+        private int index;
+
+        public ListyIterator(T[] elements)
+        {
+            this.elements = elements;
+            this.index = 0;
+        }
+
+        public bool Move()
+        {
+            if (this.index < this.elements.Length)
+            {
+                this.index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return this.index < this.elements.Length - 1 ? true : false;
+        }
+
+        public void Print()
+        {
+            if (this.elements.Length != 0)
+            {
+                Console.WriteLine(this.elements[this.index]);
+                return;
+            }
+
+            throw new InvalidOperationException("Invalid Operation!");
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.elements.Length; i++)
+            {
+                yield return this.elements[i];
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 03.IteratorsAndComparators/02.Collection/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Collection
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine()
+                .Split()
+                .Skip(1)
+                .ToArray();
+
+            ListyIterator<string> list = new ListyIterator<string>(data);
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                switch (input)
+                {
+                    case "Move":
+                        Console.WriteLine(list.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(list.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            list.Print();
+                        }
+                        catch (InvalidOperationException e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "PrintAll":
+                        Console.WriteLine(string.Join(" ", list));
+
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
