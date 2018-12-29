8de530569b1a49c1c77da20b3768f014211ec940
Gandjurov <gandjurov@abv.bg>
2018-12-29 20:00:17 +0200

    add 07.EqualityLogic

---
+++ 03.IteratorsAndComparators/07.EqualityLogic/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<Person> SortedPeople = new SortedSet<Person>();
+            HashSet<Person> peopleByHash = new HashSet<Person>();
+
+            while (n-- > 0)
+            {
+                string[] args = Console.ReadLine().Split();
+
+                Person p = new Person(args[0], int.Parse(args[1]));
+
+                SortedPeople.Add(p);
+                peopleByHash.Add(p);
+            }
+
+            Console.WriteLine(SortedPeople.Count);
+            Console.WriteLine(peopleByHash.Count);
+        }
+    }
+}
