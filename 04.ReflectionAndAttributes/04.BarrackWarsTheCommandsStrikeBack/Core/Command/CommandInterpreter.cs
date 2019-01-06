5cf42cd6e27dcd5102e098a5fe5c447fa8451255
Gandjurov <gandjurov@abv.bg>
2019-01-06 11:47:07 +0200

    add 04.BarrackWarsTheCommandsStrikeBack

---
+++ 04.ReflectionAndAttributes/04.BarrackWarsTheCommandsStrikeBack/Core/Command/CommandInterpreter.cs
@@ -0,0 +1,33 @@
+﻿namespace _03.BarrackWars.Core.Command
+{
+    using _03BarracksFactory.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private IRepository repository;
+        private IUnitFactory unitFactory;
+
+        public CommandInterpreter(IRepository repository, IUnitFactory unitFactory)
+        {
+            this.repository = repository;
+            this.unitFactory = unitFactory;
+        }
+
+        public IExecutable InterpretCommand(string[] data, string commandName)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            Type type = assembly.GetTypes()
+                    .FirstOrDefault(t => t.Name.ToLower() == commandName + "command");
+
+            var executable = (IExecutable)Activator.CreateInstance(type, new object[] { data, this.repository, this.unitFactory});
+
+            return executable;
+        }
+    }
+}
