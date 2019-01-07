5f5ad33849da0396a26be8ad4e73e10a11b5276d
Gandjurov <gandjurov@abv.bg>
2019-01-07 18:21:30 +0200

    add 08.CreateCustomClassAttribute

---
+++ 04.ReflectionAndAttributes/08.CreateCustomClassAttribute/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+[Class("Pesho", 3, "Used for C# OOP Advanced Course - Enumerations and Attributes.", "Pesho", "Svetlio")]
+public class Weapon
+{
+    protected Weapon()
+    {
+
+    }
+}
