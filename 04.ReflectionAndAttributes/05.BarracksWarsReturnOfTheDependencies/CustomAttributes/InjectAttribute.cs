2dd71550088661fbfadef4b2ce9d5b932684e699
Gandjurov <gandjurov@abv.bg>
2019-01-07 18:18:33 +0200

    add 05.BarracksWarsReturnOfTheDependencies

---
+++ 04.ReflectionAndAttributes/05.BarracksWarsReturnOfTheDependencies/CustomAttributes/InjectAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.BarracksWarsReturnOfTheDependencies.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    class InjectAttribute : Attribute
+    {
+    }
+}
