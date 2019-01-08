e75bc5c3584cca815f253435d18598b2e5dfed12
Gandjurov <gandjurov@abv.bg>
2019-01-08 21:33:54 +0200

    add 01.Axe

---
+++ 05.UnitTesting/01.Axe/ITarget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public interface ITarget
+{
+    int Health { get; }
+
+    void TakeAttack(int attackPoints);
+
+    int GiveExperience();
+
+    bool IsDead();
+}
+
