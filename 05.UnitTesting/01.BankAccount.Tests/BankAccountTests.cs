d555d663d67518169e22480e472080e483fed7c3
Gandjurov <gandjurov@abv.bg>
2019-01-08 21:35:47 +0200

    add 01.BankAccount.Tests

---
+++ 05.UnitTesting/01.BankAccount.Tests/BankAccountTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace BankAccount.Tests
+{
+    [TestFixture]
+    public class BankAccountTests
+    {
+        [Test]
+        public void ValidateDepositAmount()
+        {
+            //Arrange
+            BankAccount bankAccount = new BankAccount();
+
+            //Act
+            bankAccount.Deposit(20);
+            bankAccount.Deposit(20);
+            
+            var expectedResult = 40;
+            var actualResult = bankAccount.Amount;
+
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+        }
+    }
+}
